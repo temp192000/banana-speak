8d0066e7e47dd088afafeaf5974da776756e4bd5
temp192000 <84807334+temp192000@users.noreply.github.com>
2021-06-02 01:45:17 +0530

    update: backend up and running

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-btnTranslate.addEventListener(
-    "click",
-    function clickEventListener(){
-        console.log("Clicked!")
-        console.log("Input:",txtInput.value)
-    }
-)
+function translateURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error){
+    console.log("Some Error Occured", error);
+}
+
+function clickHandler(){
+    var text = txtInput.value;
+    fetch(translateURL(text)).then(response => response.json()).then(json => {
+        var txtTranslated = json.contents.translated;
+        output.innerText = txtTranslated;
+    }).catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click",clickHandler);
